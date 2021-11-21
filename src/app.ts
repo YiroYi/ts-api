6855ccb3d912fb0c93c85c4a1bd9cfc969b50200
YiroYi <yiroyi@live.com>
2021-11-20 23:36:35 -0600

    Complete

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
-import dotenv from 'dotenv'
-dotenv.config();
-const apiKey = process.env.MAP_BOX_API_KEY
+import axios from 'axios'
 
+const MAP_BOX_API_KEY = "putAPikey"
+
+const apiKey = MAP_BOX_API_KEY
 const form = document.querySelector('form')!;
-const addressInput = document.querySelector('address')! as HTMLInputElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+const mapIndex = document.getElementById('map')! as HTMLInputElement;
+
+declare var mapboxgl: any;
 
 function searchAdressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+
+  axios.get(
+    //`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(enteredAddress)}.json?access_token=${apiKey}`
+  ).then(data => {
+    const coordinates = data.data.features[0].geometry.coordinates
+
+    new mapboxgl.Map({
+      container: mapIndex, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [coordinates[0], coordinates[1]], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+  }).catch(err => console.log(err))
 }
 
 form.addEventListener('submit', searchAdressHandler);
