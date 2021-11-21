98cb9e8d5e682f8185698f61fdd9b627e1594c77
YiroYi <yiroyi@live.com>
2021-11-20 22:35:01 -0600

    Adding dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config();
+const apiKey = process.env.MAP_BOX_API_KEY
+
 const form = document.querySelector('form')!;
 const addressInput = document.querySelector('address')! as HTMLInputElement;
 
