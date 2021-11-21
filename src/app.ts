3a69294b1ce2cc7e1acbb78a1b42232f306858a0
YiroYi <yiroyi@live.com>
2021-11-20 22:20:43 -0600

    Adding form & input listeners

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.querySelector('address')! as HTMLInputElement;
+
+function searchAdressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+}
+
+form.addEventListener('submit', searchAdressHandler);
